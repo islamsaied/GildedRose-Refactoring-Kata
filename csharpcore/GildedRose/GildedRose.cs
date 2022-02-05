6c5d9a92c8779315e21b8926ef1b5558148a818f
Islam Saied <i.saied@qcentris.com>
2022-02-06 01:57:48 +0200

    Added some of refactoring that includes : - Sepearated method of each type of porduct. - Unit tests for UpdateQuality to verify the business requerments after refactoring.

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -14,82 +14,84 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
-                if (Items[i].SellIn < 0)
+                if (Items[i].Quality >= 0 && Items[i].Quality < 50)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
+                        Items[i] = UpdateBackstage(Items[i]);
+                    }
+                    else if (Items[i].Name == "Aged Brie")
+                    {
+                        Items[i] = UpdateAgedBrie(Items[i]);
+                    }
+                    else if (Items[i].Name == "Conjured Mana Cake")
+                    {
+                        Items[i] = UpdateConjured(Items[i]);
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        Items[i] = UpdateOthers(Items[i]);
                     }
                 }
+            }
+        }
 
-                if (Items[i].Name == "Conjured Mana Cake" && Items[i].Quality != 0)
+        public Item UpdateAgedBrie(Item item)
+        {
+            if (item.Quality < 50)
+                item.Quality = item.Quality + 1;
+            return item;
+        }
+        public Item UpdateBackstage(Item item)
+        {
+            if (item.SellIn > 10)
+            {
+                item.Quality = item.Quality + 1;
+            }
+            else if (item.SellIn < 11 && item.SellIn > 5)
+            {
+                for (int i = 0; i < 2; i++)
                 {
-                    Items[i].Quality = Items[i].Quality - 1;
+                    if (item.Quality < 50)
+                        item.Quality = item.Quality + 1;
+                }
+
+            }
+            else if (item.SellIn < 6 && item.SellIn > 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (item.Quality < 50)
+                        item.Quality = item.Quality + 1;
                 }
             }
+            else if (item.SellIn < 0)
+            {
+                item.Quality = item.Quality - item.Quality;
+            }
+            return item;
+        }
+
+        public Item UpdateConjured(Item item)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if (item.Quality > 0)
+                    item.Quality = item.Quality - 1;
+            }
+
+            return item;
+        }
+        public Item UpdateOthers(Item item)
+        {
+            if (item.Quality > 0)
+                item.Quality = item.Quality - 1;
+            return item;
         }
     }
 }
--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -7,12 +7,57 @@ namespace GildedRoseTests
     public class GildedRoseTest
     {
         [Fact]
-        public void foo()
+        public void UpdateQuality()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
+
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 },
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49 },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20 },
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 20 },
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 1},
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 49 },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 0},
+
+
+
+            };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal(19, Items[0].Quality);
+            Assert.Equal(9, Items[0].SellIn);
+            Assert.Equal(1, Items[1].Quality);
+            Assert.Equal(1, Items[1].SellIn);
+            Assert.Equal(6, Items[2].Quality);
+            Assert.Equal(4, Items[2].SellIn);
+            Assert.Equal(80, Items[3].Quality);
+            Assert.Equal(0, Items[3].SellIn);
+            Assert.Equal(21, Items[5].Quality);
+            Assert.Equal(14, Items[5].SellIn);
+            Assert.Equal(50, Items[6].Quality);
+            Assert.Equal(9, Items[6].SellIn);
+            Assert.Equal(50, Items[7].Quality);
+            Assert.Equal(4, Items[7].SellIn);
+            Assert.Equal(4, Items[8].Quality);
+            Assert.Equal(2, Items[8].SellIn);
+            Assert.Equal(22, Items[9].Quality);
+            Assert.Equal(9, Items[9].SellIn);
+            Assert.Equal(23, Items[10].Quality);
+            Assert.Equal(3, Items[10].SellIn);
+            Assert.Equal(0, Items[11].Quality);
+            Assert.Equal(9, Items[11].SellIn);
+            Assert.Equal(9, Items[12].SellIn);
+            Assert.Equal(0, Items[12].Quality);
+            Assert.Equal(0, Items[13].Quality);
+            Assert.Equal(0, Items[14].Quality);
         }
     }
 }
